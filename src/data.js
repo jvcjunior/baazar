396eb458d24d98db6cf13e51d9bcfc14148b5b36
Valter Junior <bloodsportjr@gmail.com>
2018-05-01 12:40:20 -0300

    add more itens

--- src/data.js
+++ src/data.js
@@ -28,8 +28,8 @@ export default [
   {
     id: 4,
     imgCapa: 'https://i.imgur.com/fbMYChf.jpg',
-    titulo: 'Gelágua Esmaltec',
-    subtitulo: 'R$ 220,00',
+    titulo: 'Gelágua Esmaltec (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Gelágua Esmaltec EGM30</p>,
     imgsModal: ['https://i.imgur.com/fbMYChf.jpg']
   },
@@ -44,8 +44,8 @@ export default [
   {
     id: 6,
     imgCapa: 'https://i.imgur.com/dX5Kqjs.jpg',
-    titulo: 'Fogão Brastemp',
-    subtitulo: 'R$ 500,00',
+    titulo: 'Fogão Brastemp (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Descrição: Fogão Brastemp Clean</p>,
     imgsModal: ['https://i.imgur.com/dX5Kqjs.jpg']
   },
@@ -60,24 +60,24 @@ export default [
   {
     id: 8,
     imgCapa: 'https://i.imgur.com/gykwcOj.jpg',
-    titulo: 'Sofá',
-    subtitulo: 'R$ 400,00 ',
+    titulo: 'Sofá (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Descrição: Sofá</p>,
     imgsModal: ['https://i.imgur.com/gykwcOj.jpg']
   },
   {
     id: 9,
     imgCapa: 'https://i.imgur.com/eulaw0t.jpg',
-    titulo: 'Movelzinho Projetado',
-    subtitulo: 'R$ 150,00',
+    titulo: 'Movelzinho Projetado (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Descrição: Móvel projetado</p>,
     imgsModal: ['https://i.imgur.com/eulaw0t.jpg']
   },
   {
     id: 10,
     imgCapa: 'https://i.imgur.com/GBJ839F.jpg',
-    titulo: 'Máquina de Lavar BRASTEMP',
-    subtitulo: 'R$ 800,00',
+    titulo: 'Máquina de Lavar BRASTEMP (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Descrição: Máquina de Lavar BRASTEMP</p>,
     imgsModal: ['https://i.imgur.com/GBJ839F.jpg']
   },
@@ -92,8 +92,8 @@ export default [
   {
     id: 12,
     imgCapa: 'https://i.imgur.com/3jwi3uk.jpg',
-    titulo: 'Mesa/Base projetada para cozinha',
-    subtitulo: 'R$ 100,00',
+    titulo: 'Mesa/Base projetada para cozinha (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Descrição: Mesa/Base de madeira projetada para cozinha</p>,
     imgsModal: ['https://i.imgur.com/3jwi3uk.jpg']
   },
@@ -101,7 +101,7 @@ export default [
     id: 13,
     imgCapa: 'https://i.imgur.com/hewMp9g.jpg',
     titulo: 'Mesa de jantar 4 lugares com Cadeiras',
-    subtitulo: 'R$ --',
+    subtitulo: 'R$ 400,00',
     descricao: <p>Descrição: Mesa de Jantar 4 Lugares com Cadeiras</p>,
     imgsModal: ['https://i.imgur.com/hewMp9g.jpg']
   },
@@ -117,16 +117,72 @@ export default [
     id: 15,
     imgCapa: 'https://i.imgur.com/5B1Jg6Y.jpg',
     titulo: 'TV LG Plasma 50 polegadas',
-    subtitulo: 'R$ --',
+    subtitulo: 'R$ 1000,00',
     descricao: <p>Descrição: Excelente TV LG Plasma 50 Polegadas</p>,
     imgsModal: ['https://i.imgur.com/5B1Jg6Y.jpg']
   },
   {
     id: 16,
     imgCapa: 'https://i.imgur.com/6jVgo7w.jpg',
-    titulo: 'Ar condicionado Electrolux',
-    subtitulo: 'R$ --',
+    titulo: 'Ar condicionado Electrolux (VENDIDO)',
+    subtitulo: '--',
     descricao: <p>Descrição: Ar condicionado Electrolux</p>,
     imgsModal: ['https://i.imgur.com/6jVgo7w.jpg']
+  },
+  {
+    id: 17,
+    imgCapa: 'https://i.imgur.com/c5uVPlP.jpg',
+    titulo: 'iPad Air 1 Novíssimo!!',
+    subtitulo: 'R$ 1300,00',
+    descricao: <p>Descrição: iPad Air 1 pouco usado</p>,
+    imgsModal: ['https://i.imgur.com/c5uVPlP.jpg', 'https://i.imgur.com/tP0IGQv.jpg', 'https://i.imgur.com/yHsCuZt.jpg', 'https://i.imgur.com/rZ9E3TD.jpg']
+  },
+  {
+    id: 18,
+    imgCapa: 'https://i.imgur.com/C6BU3yX.jpg',
+    titulo: 'Home Theater SAMSUNG Novíssimo!!',
+    subtitulo: 'R$ 650,00',
+    descricao: <p>Descrição: Home Theater SAMSUNG</p>,
+    imgsModal: ['https://i.imgur.com/C6BU3yX.jpg']
+  },
+  {
+    id: 19,
+    imgCapa: 'https://i.imgur.com/1sRmkoY.jpg',
+    titulo: 'Frigideira',
+    subtitulo: 'R$ 50,00',
+    descricao: <p>Descrição: Frigideira</p>,
+    imgsModal: ['https://i.imgur.com/1sRmkoY.jpg']
+  },
+  {
+    id: 20,
+    imgCapa: 'https://i.imgur.com/2Oots34.jpg',
+    titulo: 'Travessa de Vidro I' ,
+    subtitulo: 'R$ 15,00',
+    descricao: <p>Descrição: Travessa de Vidro</p>,
+    imgsModal: ['https://i.imgur.com/2Oots34.jpg']
+  },
+  {
+    id: 21,
+    imgCapa: 'https://i.imgur.com/jBmTP4Z.jpg',
+    titulo: 'Travessa de Vidro II',
+    subtitulo: 'R$ 15,00',
+    descricao: <p>Descrição: Travessa de Vidro</p>,
+    imgsModal: ['https://i.imgur.com/jBmTP4Z.jpg']
+  },
+  {
+    id: 22,
+    imgCapa: 'https://i.imgur.com/Eu5Lunk.jpg',
+    titulo: 'Conjunto de panelas INOX',
+    subtitulo: 'R$ 100,00',
+    descricao: <p>Descrição: Conjunto de panelas INOX</p>,
+    imgsModal: ['https://i.imgur.com/Eu5Lunk.jpg']
+  },
+  {
+    id: 23,
+    imgCapa: 'https://i.imgur.com/4Y1Cjc7.jpg',
+    titulo: 'Aspirador de pó Britania',
+    subtitulo: 'R$ 80,00',
+    descricao: <p>Descrição: Aspirador de pó Britania</p>,
+    imgsModal: ['https://i.imgur.com/4Y1Cjc7.jpg']
   },
 ];
